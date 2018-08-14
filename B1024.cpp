33f8a73a308827219feb5978d30b74bb28c1a3d9
macshun <36837727+macshun@users.noreply.github.com>
2018-08-14 21:57:29 +0800

    Delete 1.txt

--- B1024.cpp
+++ B1024.cpp
@@ -7,9 +7,58 @@
 */
 #include <iostream>
 #include <cstdio>
+#include <cstring>
+#include <cmath>
 using namespace std;
+const int max_n = 10010;
+char a[max_n];
 int main()
 {
+	scanf("%s",a);
+	if(a[0] == '-')
+		printf("-");
+	int len = strlen(a);
+	int i,z = 0,pro = 1;
+	for(i = len - 1; i >= 1; i--){
+		if(a[i] != '+' && a[i] != '-'){
+			z += (a[i] - '0')*pro;
+			pro = pro*10;
+		}else
+			break;
+	}
+		if(z == 0){
+			for(int k = 1; k < len; k++){
+				if(a[k] == 'E')
+					break;
+				printf("%c",a[k]);
+			}
+		}
+		
+		if(a[i] == '+'){
+			int num = 0;
+			printf("%c",a[1]);
+			for(int j = 3;j < i-1; j++){
+				num++;
+				printf("%c",a[j]);
+				if(i-4 > z && z == num){
+					printf(".");
+				}
+				
+			}
+			for(int j = 0;j < z-(i-4); j++){
+				printf("0");
+			}
+		}
+		if(a[i] == '-'){
+			printf("0.");
+			for(int j = 0; j < z-1; j++){
+				printf("0");
+			}
+			printf("%c",a[1]);
+			for(int j = 3; j < i-1; j++){
+				printf("%c",a[j]);
+			}
+		}
 	
 	return 0;
 }
